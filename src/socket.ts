dc5aa3888275dd004b5cf418322c4b2ecc307d85
Jairo Alcivar <jairoalc@espol.edu.ec>
2024-05-09 22:05:37 -0500

    Comentarios

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,10 @@
 import { io } from 'socket.io-client';
 
-// export const socketURL = process.env.NODE_ENV === 'production' ? '' : 'ws://127.0.0.1:3000/caballos';
-export const socketURL = process.env.NODE_ENV === 'production' ? '' : 'https://pressure-laos-suppose-lp.trycloudflare.com/caballos';
+//Para que funcione online comenta esta linea
+export const socketURL = process.env.NODE_ENV === 'production' ? '' : 'ws://127.0.0.1:3000/caballos';
+
+//Para que funcione online descomenta esta linea y agrega el link de cloudflare
+// export const socketURL = process.env.NODE_ENV === 'production' ? '' : '<cloudflare link>/caballos';
 
 export const socket = io(socketURL, {
   autoConnect: false
